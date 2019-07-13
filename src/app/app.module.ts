935f64105f752b5948738f201bfd6dcd5d5b556b
kbreczko <kamil.breczko@gmail.com>
2019-07-13 21:07:29 +0200

    add HeroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './component/heroes/heroes.component';
+import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,

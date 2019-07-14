431233dd728a0c798176b4cae04ac00dd7773ec5
kbreczko <kamil.breczko@gmail.com>
2019-07-14 19:02:06 +0200

    search by name

--- src/app/component/hero-search/hero-search.component.ts
+++ src/app/component/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators'
+import {Hero} from "../../models/hero";
+import {HeroService} from "../../services/hero.service";
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
 }

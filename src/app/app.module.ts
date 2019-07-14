65bfa5c68912792c685c855f7ce03eaf582427ac
kbreczko <kamil.breczko@gmail.com>
2019-07-14 13:51:41 +0200

    add InMemoryDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './component/heroes/heroes.component';
-import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
-import { MessagesComponent } from './component/messages/messages.component';
-import { DashboardComponent } from './component/dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './component/heroes/heroes.component';
+import {HeroDetailComponent} from './component/hero-detail/hero-detail.component';
+import {MessagesComponent} from './component/messages/messages.component';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +21,11 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

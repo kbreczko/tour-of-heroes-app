f337902e1ac81883fefd09645c41b7b41e578b57
kbreczko <kamil.breczko@gmail.com>
2019-07-14 13:45:56 +0200

    routable HeroDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HeroesComponent} from "./component/heroes/heroes.component";
 import {DashboardComponent} from "./component/dashboard/dashboard.component";
+import {HeroDetailComponent} from "./component/hero-detail/hero-detail.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({

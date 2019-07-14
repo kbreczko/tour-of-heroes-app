124afb84a0b7b62fd9e7a0a05159b14780445307
kbreczko <kamil.breczko@gmail.com>
2019-07-14 13:12:04 +0200

    add a dashboard view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HeroesComponent} from "./component/heroes/heroes.component";
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
